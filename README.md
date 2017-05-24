1ed3c656be425a7227049d2d62d0203d34bf8563
pjvalentini <pj.valentini@gmail.com>
2017-05-23 22:01:20 -0400

    Updated comments and README.md

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # Dice-App-Version-2.0
+
+// *** DICE APP Version 2.0 - updated code to reflect animations of the dice,
+
+// Problem...my nieces are always loosing the dice to their board games.
+// Solution...create a virtual roll of the dice using a computer.
+
+This project has HTML, CSS and JavaScript Pages.
--- javascript/script.js
+++ javascript/script.js
@@ -1,37 +1,49 @@
+// *** DICE APP Version 2.0 - updated code to reflect aninations on the dice,
+
+// Problem...my nieces are always loosing the dice to their board games.
+// Solution...create a virtual roll of the dice using a computer.
+
+// This Function allow for a random number to be generated on the dice roll,
+// When clicking the dice.jpg img.
 function getRandomNumber(min, max) {
-    return Math.floor(Math.random() * max) + min;
+	return Math.floor(Math.random() * max) + min;
 }
 
+// These vars select the elements from the HTMl Page.
 var img = document.querySelector(".dice");
 var dice1 = document.querySelector(".dice1");
 var dice2 = document.querySelector(".dice2");
 var rollValue = document.querySelector(".rollValue");
 
 function roll(num1, num2) {
-    dice1.setAttribute('data-face', num1);
-    dice2.setAttribute('data-face', num2);
+	dice1.setAttribute('data-face', num1);
+	dice2.setAttribute('data-face', num2);
 }
 
+// Anunimation function, creates dynamic dice rolling, setting the speed,
+// and interval between dice rolls.
 function animate(final) {
-    var times = 20;
-    var speed = 100;
-    var intv = setInterval(function() {
-        if (times <= 0) {
-            clearInterval(intv);
-            final();
-            return;
-        }
-        roll(getRandomNumber(1, 6), getRandomNumber(1, 6)); 
-        times--;
-    }, speed);
+	var times = 20;
+	var speed = 100;
+	var intv = setInterval(function() {
+		if (times <= 0) {
+			clearInterval(intv);
+			final();
+			return;
+		}
+		roll(getRandomNumber(1, 6), getRandomNumber(1, 6));
+		times--;
+	}, speed);
 }
-
+// This onclick function makes it so now you can click on the dice img and get
+// a response -- the dice roll with movement and now display the values of both
+// dice.
 img.onclick = function() {
-    animate(function(){
-        var result = parseInt(dice1.getAttribute('data-face')) + 
-                     parseInt(dice2.getAttribute('data-face'));
-        rollValue.innerText = result;                   
-    });
+	animate(function(){
+		var result = parseInt(dice1.getAttribute('data-face')) +
+								 parseInt(dice2.getAttribute('data-face'));
+		rollValue.innerText = result;
+	});
 };
 
 img.onclick();
